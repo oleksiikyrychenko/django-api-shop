7d1089d4b3f7f5f018fdaa7aa952aac7599c14e9
alexey1452 <kirickenko1452@gmail.com>
2020-02-28 20:07:32 +0200

    login functionality

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('users/', views.UserView.as_view()),
-    path('users/<int:pk>', views.UserView.as_view())
+    url(r'^login/$', views.LoginView.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -1,18 +1,28 @@
+import jwt
+from django.contrib.auth import user_logged_in
+from django.forms import model_to_dict
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_jwt.serializers import jwt_payload_handler
 
+from shop_api import settings
 from .models import Role, Profile
 from .serializers import UserSerializers, RoleSerializers
+from django.contrib.auth import authenticate
 
 
 class UserView(APIView):
+    @permission_classes([IsAuthenticated, ])
     def get(self, request):
         users = Profile.objects.all()
         serializer = UserSerializers(users, many=True)
         return Response({"users": serializer.data})
 
+    @permission_classes([AllowAny, ])
     def post(self, request):
         user = request.data.get('user')
         serializer = UserSerializers(data=user)
@@ -23,7 +33,9 @@ class UserView(APIView):
             user_saved.save()
         return Response({"user": serializer.data})
 
-    def put(self, request, pk):
+    @permission_classes([IsAuthenticated, ])
+    def put(self, request):
+        pk = request.query_params.get('pk')
         user = get_object_or_404(Profile.objects.all(), pk=pk)
         data = request.data.get('user')
         serializer = UserSerializers(instance=user, data=data, partial=True)
@@ -31,10 +43,32 @@ class UserView(APIView):
             serializer.save()
         return Response({"user": serializer.data})
 
-    def delete(self, request, pk):
+    @permission_classes([IsAuthenticated, ])
+    def delete(self, request):
+        pk = request.query_params.get('pk')
         article = get_object_or_404(Profile.objects.all(), pk=pk)
         article.delete()
         return Response({"message": "User has been deleted."})
+
+
+class LoginView(APIView):
+    @permission_classes([AllowAny, ])
+    def post(self, request):
+        if 'email' in request.data and 'password' in request.data:
+            user = authenticate(username=request.data['email'], password=request.data['password'])
+            try:
+                payload = jwt_payload_handler(user)
+                token = jwt.encode(payload, settings.SECRET_KEY)
+                user_details = {}
+                user_details['token'] = token
+                user_details['user'] = model_to_dict(user)
+                user_logged_in.send(sender=user.__class__, request=request, user=user)
+                return Response(user_details, status=status.HTTP_200_OK)
+
+            except Exception as e:
+                raise e
+        else:
+            return Response('Ah oh, not user or password in body', status=status.HTTP_400_BAD_REQUEST)
 
 
 class RoleView(viewsets.ModelViewSet):
